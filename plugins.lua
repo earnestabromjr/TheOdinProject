66995ec23eeeb3fd2cdcd1d8a2fb882d88726b66
earnestabromjr <abromearnest@gmail.com>
2023-10-27 17:57:45 -0500

    Added Lists

---
+++ plugins.lua
@@ -0,0 +1,14 @@
+-- plugins.lua 
+
+return {
+
+    -- Nvimtree (File Explorer)
+    -- Added this reference to the initial file
+    {
+        'nvim-tree/nvim-tree.lua',
+        lazy = true,
+        dependencies = {
+            'nvim-tree/nvim-web-devicons'
+        }
+    }
+}
